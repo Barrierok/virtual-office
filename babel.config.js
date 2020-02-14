74e7cad55ed8c344c2c854e934bb29d9ddd554da
Ilya Zubtsov <ilyazubtsov@gmail.com>
2020-02-14 19:52:30 +0300

    IZ-6 Отладил проект для старта

--- babel.config.js
+++ babel.config.js
@@ -6,5 +6,10 @@ module.exports = {
   plugins: [
     ['@babel/plugin-proposal-decorators', { legacy: true }],
     ['@babel/plugin-proposal-class-properties', { loose: true }],
+    ['@babel/plugin-transform-runtime',
+      {
+        regenerator: true,
+      },
+    ],
   ],
 };
