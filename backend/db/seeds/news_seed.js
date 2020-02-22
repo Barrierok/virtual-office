fcc14a520619c939e165cddbc4ef78ad1d5aa508
Ilya Zubtsov <ilyazubtsov@gmail.com>
2020-02-22 11:17:45 +0300

    Revert "Merge pull request #21 from Barrierok/revert-19-CI"
    This reverts commit 3cf7c7901b9381d1b8558765a8b79a4eed5d0fb4, reversing
    changes made to 610ea6f36fdfd62a5550cfb269175ca9c7415bab.

--- backend/db/seeds/news_seed.js
+++ backend/db/seeds/news_seed.js
@@ -1,5 +1,5 @@
 
-exports.seed = knex => knex('feeds').del()
+exports.seed = (knex) => knex('feeds').del()
   .then(() => (knex('feeds').insert([
     {
       title: 'Hello, dude', body: 'Example post for testing app', author: 'Pavel', archieve: false, created_at: '2019-01-13 23:12:11',
--- knexfile.js
+++ knexfile.js
@@ -14,30 +14,13 @@ const connection = {
 };
 
 module.exports = {
-  development: {
-    client: 'pg',
-    connection: {
-      database: process.env.DB,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-    },
-    migrations: {
-      directory: path.join(BASE_PATH, 'migrations'),
-    },
-    seeds: {
-      directory: path.join(BASE_PATH, 'seeds'),
-    },
-    ...knexSnakeCaseMappers(),
+  client: 'pg',
+  connection: process.env.DATABASE_URL || connection,
+  migrations: {
+    directory: path.join(BASE_PATH, 'migrations'),
   },
-  production: {
-    client: 'pg',
-    connection: process.env.DATABASE_URL,
-    migrations: {
-      directory: path.join(BASE_PATH, 'migrations'),
-    },
-    seeds: {
-      directory: path.join(BASE_PATH, 'seeds'),
-    },
-    ...knexSnakeCaseMappers(),
-  }
+  seeds: {
+    directory: path.join(BASE_PATH, 'seeds'),
+  },
+  ...knexSnakeCaseMappers(),
 };
