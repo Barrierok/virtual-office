bf72a35b45cec3bd6cf8141d2a7c8a5960ec5d09
AlexandrKoliukh <alexkoliukh@gmail.com>
2020-02-24 14:27:30 +0300

    AK-3 создание колонки

---
+++ backend/db/migrations/20200224113531_columns.js
@@ -0,0 +1,13 @@
+exports.up = (knex) => (
+  knex.schema
+    .createTable('columns', (table) => {
+      table.increments('id').primary();
+      table.string('title').unique();
+      table.datetime('created_at');
+    })
+);
+
+exports.down = (knex) => (
+  knex.schema
+    .dropTableIfExists('columns')
+);
