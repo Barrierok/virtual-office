c3003024f3650578e578fe82a560758def7c1509
AlexandrKoliukh <alexkoliukh@gmail.com>
2020-05-24 09:55:24 +0300

    лишняя миграция

--- backend/db/migrations/20200505191315_new_news.js
+++
@@ -1,9 +0,0 @@
-exports.up = (knex) =>
-  knex.schema.table('feeds', (table) => {
-    table.text('body');
-  });
-
-exports.down = (knex) =>
-  knex.schema.table('feeds', (table) => {
-    table.dropColumn('body');
-  });
